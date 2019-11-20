da434de8e465a7587a4dad51c69b6b04dcc7c491
Ivan Galakhov <igalakhov00@stuy.edu>
2019-11-20 08:27:44 -0500

    yea

--- app/models.py
+++ app/models.py
@@ -3,8 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+user_card_association = db.Table(
+    'user_card_association',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('card_id', db.Integer, db.ForeignKey('card.id'))
+)
 
-class Users(db.Model, UserMixin):
+
+class User(db.Model, UserMixin):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -12,6 +18,7 @@ class Users(db.Model, UserMixin):
     balance = db.Column(db.Integer, nullable=False)
 
     # relationships
+    cards = db.relationship('Card', secondary=user_card_association)
 
     def __init__(self, username, password):
         self.username = username
@@ -19,7 +26,7 @@ class Users(db.Model, UserMixin):
         self.balance = 1000
 
 
-class Cards(db.Model):
+class Card(db.Model):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
@@ -28,6 +35,7 @@ class Cards(db.Model):
     type_id = db.Column(db.Integer, nullable=False)
 
     # relationships
+    users = db.relationship('User', secondary=user_card_association)
 
     def __init__(self, name, picture_url, description, type):
         self.name = name
@@ -35,12 +43,16 @@ class Cards(db.Model):
         self.description = description
         self.type_id = type.id
 
-class Trades(db.Model):
-    #columns
-    id = db.Column(db.Integer, primary_key=True)
-    request_id = db.Column(db.Integer, nullable=False)
-    given_id = db.Column(db.Integer, nullable=False)
 
-    def __init__(self,request_id,given_id):
+class Trade(db.Model):
+    # columns
+    id = db.Column(db.Integer, primary_key=True)
+    request_card_id = db.Column(db.ForeignKey('card.id'))
+    given_card_id = db.Column(db.ForeignKey('card.id'))
+
+    request_card = db.relationship('Card', foreign_keys=[request_card_id])
+    given_card = db.relationship('Card', foreign_keys=[given_card_id])
+
+    def __init__(self, request_id, given_id):
         self.request_id = request_id
-        self.given_id = given_idg
+        self.given_id = given_id
