771f8b7561c3f8028f6927337d2fa2014b319313
Xyresic <ericjmlam@gmail.com>
2019-11-20 08:14:03 -0500

    Added trades table

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model, UserMixin):
+class Users(db.Model, UserMixin):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -19,7 +19,7 @@ class User(db.Model, UserMixin):
         self.balance = 1000
 
 
-class Card(db.Model):
+class Cards(db.Model):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
@@ -34,3 +34,13 @@ class Card(db.Model):
         self.picture_url = picture_url
         self.description = description
         self.type_id = type.id
+
+class Trades(db.Model):
+    #columns
+    id = db.Column(db.Integer, primary_key=True)
+    request_id = db.Column(db.Integer, nullable=False)
+    given_id = db.Column(db.Integer, nullable=False)
+
+    def __init__(self,request_id,given_id):
+        self.request_id = request_id
+        self.given_id = given_idg
