777f21b5a464f520c9a16e5bd94fe3ce25d91f6a
Ivan Galakhov <igalakhov00@stuy.edu>
2019-11-22 11:21:57 -0500

    started work on timezones

---
+++ app/timezones.py
@@ -0,0 +1,7 @@
+# this file provides functionality for converting money to relevant currency
+
+class Currency:
+
+    def __init__(self, name rate):
+        self.name = name
+        self.rate = rate
