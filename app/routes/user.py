b065275a7044f47a80721d625f1dcde7411123a3
Xyresic <ericjmlam@gmail.com>
2019-11-22 08:02:21 -0500

    Added more featured cards

--- app/routes/user.py
+++ app/routes/user.py
@@ -29,6 +29,7 @@ def mycards():
 @user.route('/marketplace')
 @login_required
 def marketplace():
-    featured = ['xy6-61','xy8-63','xy8-64','xy2-69','xy2-13']
-    featured = [get_card(n) for n in featured]
+    f = ['xy6-61','xy8-63','xy8-64','xy2-69','xy2-13','sm5-161','sm5-163','sm6-140','smp-SM210','sm11-247']
+    f = [get_card(n) for n in f]
+    featured = [f[:5],f[5:]]
     return render_template('marketplace.html', featured=featured, new=[], popular=[], value=[])
