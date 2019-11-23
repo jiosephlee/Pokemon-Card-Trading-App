7d29404edccb80b088430f3db3d59929e60253e3
Xyresic <ericjmlam@gmail.com>
2019-11-23 16:18:28 -0500

    Changed routes

--- app/routes/user.py
+++ app/routes/user.py
@@ -32,9 +32,9 @@ def mycards():
     return render_template('mycards.html', card = c)
     # cards = current_user.cards)
 
-@user.route('/marketplace')
+@user.route('/marketplace/cards')
 @login_required
-def marketplace():
+def cards():
     f = ['xy6-61','xy8-63','xy8-64','xy2-69','xy2-13','sm5-161','sm5-163','sm6-140','sm11-247','smp-SM210']
     f = [get_card(n) for n in f]
     featured = [f[:5],f[5:]]
@@ -44,9 +44,14 @@ def marketplace():
     new = [n[:5],n[5:]]
     p = Card.query.order_by(Card.num_sales)[:10]
     popular = [p[:5],p[5:]]
-    return render_template('marketplace.html', featured=featured, new=new, popular=popular)
+    return render_template('cards.html', featured=featured, new=new, popular=popular)
 
-@user.route('/marketplace/trade')
+@user.route('/marketplace/packs')
 @login_required
-def trade():
+def packs():
+    return ''
+
+@user.route('/marketplace/trades')
+@login_required
+def trades():
     return ''
