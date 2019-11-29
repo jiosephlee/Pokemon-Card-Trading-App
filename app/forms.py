258d3a02c19b5502b9a56365cfd4f3b15bcb19fe
Ivan Galakhov <igalakhov00@stuy.edu>
2019-11-29 15:13:44 -0500

    search done

--- app/forms.py
+++ app/forms.py
@@ -13,6 +13,13 @@ def get_rarity_options():
     return sorted(rarity_options)
 
 
+def get_type_options():
+    all_cards = Card.query.all()
+    all_types = list(set([i.type for i in all_cards]))
+    types_options = [(i, i) for i in all_types if i]
+    return sorted(types_options)
+
+
 RARITY_OPTIONS = [('Common', 'Common'), ('LEGEND', 'LEGEND'), ('Rare', 'Rare'),
                   ('Rare ACE', 'Rare ACE'), ('Rare BREAK', 'Rare BREAK'),
                   ('Rare Holo', 'Rare Holo'), ('Rare Holo EX', 'Rare Holo EX'),
@@ -22,6 +29,34 @@ RARITY_OPTIONS = [('Common', 'Common'), ('LEGEND', 'LEGEND'), ('Rare', 'Rare'),
                   ('Rare Rainbow', 'Rare Rainbow'),
                   ('Rare Secret', 'Rare Secret'), ('Rare Ultra', 'Rare Ultra'),
                   ('Shining', 'Shining'), ('Uncommon', 'Uncommon')]
+
+TYPE_OPTIONS = [('Colorless', 'Colorless'),
+                ('Colorless,Psychic', 'Colorless,Psychic'),
+                ('Darkness', 'Darkness'),
+                ('Darkness,Darkness', 'Darkness,Darkness'),
+                ('Darkness,Metal', 'Darkness,Metal'),
+                ('Darkness,Psychic', 'Darkness,Psychic'), ('Dragon', 'Dragon'),
+                ('Fairy', 'Fairy'), ('Fairy,Psychic', 'Fairy,Psychic'),
+                ('Fairy,Water', 'Fairy,Water'), ('Fighting', 'Fighting'),
+                ('Fighting,Darkness', 'Fighting,Darkness'),
+                ('Fighting,Metal', 'Fighting,Metal'), ('Fire', 'Fire'),
+                ('Fire,Darkness', 'Fire,Darkness'),
+                ('Fire,Grass', 'Fire,Grass'),
+                ('Fire,Lightning', 'Fire,Lightning'),
+                ('Fire,Metal', 'Fire,Metal'), ('Fire,Water', 'Fire,Water'),
+                ('Grass', 'Grass'), ('Grass,Darkness', 'Grass,Darkness'),
+                ('Grass,Metal', 'Grass,Metal'), ('Lightning', 'Lightning'),
+                ('Lightning,Darkness', 'Lightning,Darkness'),
+                ('Lightning,Grass', 'Lightning,Grass'),
+                ('Lightning,Metal', 'Lightning,Metal'),
+                ('Lightning,Water', 'Lightning,Water'), ('Metal', 'Metal'),
+                ('Metal,Darkness', 'Metal,Darkness'),
+                ('Metal,Fighting', 'Metal,Fighting'), ('Psychic', 'Psychic'),
+                ('Psychic,Darkness', 'Psychic,Darkness'),
+                ('Psychic,Metal', 'Psychic,Metal'), ('Water', 'Water'),
+                ('Water,Darkness', 'Water,Darkness'),
+                ('Water,Fighting', 'Water,Fighting'),
+                ('Water,Fire', 'Water,Fire'), ('Water,Metal', 'Water,Metal')]
 
 
 # special field for selecting multiple things
@@ -59,6 +94,7 @@ class LogInForm(FlaskForm):
 
 
 class SearchForm(FlaskForm):
-    rarities = MultiCheckboxField('Rarities:', choices=RARITY_OPTIONS)
+    rarities = MultiCheckboxField('Filter by rarity:', choices=RARITY_OPTIONS)
+    types = MultiCheckboxField('Filter by type:', choices=TYPE_OPTIONS)
     search = StringField('Query:', validators=[Length(min=0, max=80)])
     submit = SubmitField('Search')
